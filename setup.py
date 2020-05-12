aece83b586f1718bac5618db9f77647781c1ae37
akanksharathore <rathore.aakanksha58@gmail.com>
2020-05-12 21:57:07 +0530

    setup complete

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'mothe library facilitates detecting and tracking animals in heterogeneous ecological space by using a neural network to study collective behaviour.',   # Give a short description about your library
-  author = 'YOUR NAME',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  author = 'Akanksha Rathore, Colin Torney, Vishwesha Guttal, Nitika Sharma, Ananth Sharma',                   # Type in your name
+  author_email = 'rathore.aakanksha58@gmail.com, colin.torney@glasgow.ac.uk, guttal@iisc.ac.in, nitikasharma@g.ucla.edu, ananth.sharmacs@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/tee-lab/mothe',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/tee-lab/mothe/archive/0.1.tar.gz',    # I explain this later on
   keywords = ['object detection', 'object tracking', 'kalman filter', 'multiple object detection', 'dataset generation', 'computer vision', 'annotation', 'classification', 'CNN'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'yaml',
@@ -39,5 +39,6 @@ setup(
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
   ],
 )
